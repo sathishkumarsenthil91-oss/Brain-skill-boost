7518a6fdf037dfd33c623886e6daa25cb82fb759
Sathish Kumar S <sathishkumarsenthil91@gmail.com>
2026-09-16 22:26:55 +0530

    Use bundled auth helper for registration

--- api/auth/register.ts
+++ api/auth/register.ts
@@ -1,4 +1,4 @@
-import { createRequestAuthClient, requestOrigin, requireAppRequest, setNoStore } from '../../server-lib/session';
+import { createRequestAuthClient, requestOrigin, requireAppRequest, setNoStore } from '../_lib/session.js';
 
 export default async function handler(req: any, res: any) {
   setNoStore(res);
